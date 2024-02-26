489056120e5c1290682c3687a0e6557e3b571914
Berkpigh <pierre.berkovits@netplus.ch>
2024-02-26 19:09:08 +0100

    transactionModel

--- server/database/models/transactionModel.js
+++ server/database/models/transactionModel.js
@@ -2,12 +2,7 @@ const { ObjectId } = require('mongodb')
 const mongoose = require('mongoose')
 
 const transactionSchema = new mongoose.Schema({
-    accountType: {
-        type: Number,
-        required: false,
-        unique: false,
-    },
-    userId: {
+    accountId: {
         type : String,
         required: true,
         unique: false,
@@ -17,12 +12,35 @@ const transactionSchema = new mongoose.Schema({
         required: true,
         unique: false,
     },
-    name: {
+    description: {
         type: String,
         required: true,
+        unique: false,
+    },
+    amount: {
+        type: Number,
+        required: true,
+        unique: false,
     },
     balance: {
         type: Number,
+        required: true,
+        unique: false,
+    },
+    transactionType: {
+        type: Number,
+        required: false,
+        unique: false,
+    },
+    transactionCategory: {
+        type: Number,
+        required: false,
+        unique: false,
+    },
+    notes: {
+        type: Number,
+        required: false,
+        unique: false,
     },
 }, { timestamps: true })
 
